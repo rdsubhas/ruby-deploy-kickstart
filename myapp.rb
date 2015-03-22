fbf2857f1608e6d3425ec2ec87e35ad50fdb6900
rdsubhas <rdsubhas@gmail.com>
2015-03-22 15:25:38 +0530

    Added production provisioning

--- myapp.rb
+++ myapp.rb
@@ -4,16 +4,24 @@ require 'rack-mini-profiler' if development?
 require 'json'
 
 class Myapp < Sinatra::Base
+
   configure :development do
+    # Enable hot reloading in development mode
     register Sinatra::Reloader
     use Rack::MiniProfiler
   end
 
-  set :bind, '0.0.0.0'
+  # Enable request logging
   set :logging, true
 
+  # If you're planning to proxy behind nginx,
+  # Then you can move this bind inside `configure :development`
+  set :bind, '0.0.0.0'
+
+  # A simple endpoint
   get '/' do
     config = ENV.select{ |k,v| k =~ /^MYAPP_/ }
     return "Running with config: #{config.inspect}"
   end
+
 end
